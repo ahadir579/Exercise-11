eee900097317ef404b6e8fde048c4a1638941d6a
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-08-03 23:23:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ['Ali Phull', 'Talha Phull', 'Usama Phull', 'Hamza Phull', 'Asad Phull'];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
